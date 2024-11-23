4d4b8e8089e12637695f81aabe65b505035425bd
Alchemmist <anton.ingrish@gmail.com>
2024-11-23 19:18:23 +0300

    remove some kringe code

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-def very_friendly_greeting():
-    for i in range(5):
-        print("Hello!")
+# def very_friendly_greeting():
+#     for i in range(5):
+#         print("Hello!")
 
 
 def main():
     print('My first git program')
-    very_friendly_greeting()
+    # very_friendly_greeting()
 
 
 if __name__ == '__main__':
